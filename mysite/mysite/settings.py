b438ea1e8e83f4cdd0942b97c8d1d5c911befebb
zouxinzh <60423610+zouxinzh@users.noreply.github.com>
2020-02-02 18:10:31 +0800

    adjusted the navbar

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -120,6 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'shunli/static/css/style.css',
+    'shunli/static/css/test.css',
+    'shunli/static/page.js',
+    'shunli/static/assets',
+]
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
